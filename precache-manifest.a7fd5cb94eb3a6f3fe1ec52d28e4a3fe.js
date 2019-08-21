24c1769246a7a25603106089c4b7a71f4b2bd61e
Iain MacDonald <iain.ilearner@gmail.com>
2019-08-21 18:42:31 +0800

    Updates

---
+++ precache-manifest.a7fd5cb94eb3a6f3fe1ec52d28e4a3fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ebb3195eec9d457a036c5737a1f957f",
+    "url": "/flight-tracker/index.html"
+  },
+  {
+    "revision": "7fb42c3bf19c7ce76e87",
+    "url": "/flight-tracker/static/css/main.4b17b976.chunk.css"
+  },
+  {
+    "revision": "3a63b31b658656f14ef3",
+    "url": "/flight-tracker/static/js/2.a8f91875.chunk.js"
+  },
+  {
+    "revision": "7fb42c3bf19c7ce76e87",
+    "url": "/flight-tracker/static/js/main.0951f2ba.chunk.js"
+  },
+  {
+    "revision": "736e31592bc95c393d3f",
+    "url": "/flight-tracker/static/js/runtime~main.55f9c0e0.js"
+  },
+  {
+    "revision": "d32ef63bcba0338254815a592138d515",
+    "url": "/flight-tracker/static/media/map_outline.d32ef63b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.891d35324eae81c6c4d7d0eb68d2588a.js"
+  "/flight-tracker/precache-manifest.a7fd5cb94eb3a6f3fe1ec52d28e4a3fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flight-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
