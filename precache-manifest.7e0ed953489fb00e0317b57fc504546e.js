e41efe21c4aef22f6273590c553fe2d1f01dd2e1
Iain MacDonald <iain.ilearner@gmail.com>
2019-08-22 18:33:02 +0800

    Updates

---
+++ precache-manifest.7e0ed953489fb00e0317b57fc504546e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10d2ce7267cdcd66a7c0a172e2a1a65",
+    "url": "/flight-tracker/index.html"
+  },
+  {
+    "revision": "cb992daae97902f8db6f",
+    "url": "/flight-tracker/static/css/main.4b17b976.chunk.css"
+  },
+  {
+    "revision": "1a9128e5b8e1dd003797",
+    "url": "/flight-tracker/static/js/2.4e716a1f.chunk.js"
+  },
+  {
+    "revision": "deaad22fbbdcb6182133",
+    "url": "/flight-tracker/static/js/3.22eb4858.chunk.js"
+  },
+  {
+    "revision": "cb992daae97902f8db6f",
+    "url": "/flight-tracker/static/js/main.103f7c56.chunk.js"
+  },
+  {
+    "revision": "d91514fefd1e942d2095",
+    "url": "/flight-tracker/static/js/runtime~main.6dca72d7.js"
+  }
+]);
